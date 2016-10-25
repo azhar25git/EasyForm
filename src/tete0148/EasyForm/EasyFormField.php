8d70c61957fea57f3ad93c4e1f966bf137b465ff
Theo <theoc.01@live.fr>
2016-10-25 22:37:13 +0200

    Create EasyFormField class's structure

--- src/tete0148/EasyForm/EasyFormField.php
+++ src/tete0148/EasyForm/EasyFormField.php
@@ -6,6 +6,7 @@ class EasyFormField {
 
     private $name;
     private $type;
+    private $attributes = [];
 
     /**
      * EasyFormField constructor.
@@ -18,4 +19,53 @@ class EasyFormField {
         $this->type = $type;
     }
 
+    /**
+     * Set attributes array
+     *
+     * @param $attributes
+     */
+    public function setAttributes($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Add an attribute
+     *
+     * @param $attribute
+     * @param $value
+     */
+    public function addAttribute($attribute, $value)
+    {
+        $this->attributes[$attribute] = $value;
+    }
+
+    /**
+     * Set attribute class
+     *
+     * @param $class
+     */
+    public function setClass($class)
+    {
+        $this->attributes['class'] = $class;
+    }
+
+    /**
+     * Set field id
+     *
+     * @param $id
+     */
+    public function setId($id)
+    {
+        $this->attributes['id'] = $id;
+    }
+
+    /**
+     * Make field required
+     *
+     */
+    public function required()
+    {
+        $this->attributes['required'] = 'required';
+    }
 }
