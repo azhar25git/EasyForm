31913d664c59d12fd04ac1fd2afecc7aa7e2e04d
Theo <theoc.01@live.fr>
2016-10-25 22:44:34 +0200

    Create FieldTypes enum

---
+++ src/tete0148/EasyForm/EasyFormFieldTypes.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace tete0148\EasyForm;
+
+class EasyFormFieldTypes {
+
+    private static $types = [
+        'button',
+        'checkbox',
+        'color',
+        'date',
+        'datetime',
+        'datetime-local',
+        'email',
+        'file',
+        'hidden',
+        'image',
+        'month',
+        'number',
+        'password',
+        'radio',
+        'range',
+        'reset',
+        'search',
+        'submit',
+        'tel',
+        'text',
+        'time',
+        'url',
+        'week',
+        'textarea'
+    ];
+
+    public static function getTypes()
+    {
+        return self::types;
+    }
+}
